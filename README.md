d6648638384350cd29c94cfc1fc4f7440702da2b
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-10-16 17:50:15 +0500

    Set up project
    Set up the whole directory structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Backend
+---
+
+## To run locally (without Docker container)
+
+1. Clone the repository
+2. Navigate to the root folder of the project
+3. Create virtual environmemnt: `python -m venv .venv`
+4. Switch to virtual environment: `source .venv/scripts/activate`
+5. Install dependencies: `pip install -r requirements.txt`
+6. Run the application: `uvicorn api.main:app --reload`
---
+++ api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+# Create an instance of the FastAPI class
+app = FastAPI()
+
+# Define a route for the root URL ("/")
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"message": "Hello World"}
+
+# Run the application using 'uvicorn' if this script is run directly
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000) # uvicorn api.main:app --reload
